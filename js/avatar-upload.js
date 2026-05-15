3c3bc5324d5c628757f53fedaca2a4f2d32b6917
Luminoss <luminoss082@gmail.com>
2026-05-15 21:58:49 +0200

    cc

--- js/avatar-upload.js
+++ js/avatar-upload.js
@@ -1,5 +1,6 @@
 import { supabaseClient } from "./config.js";
 
+
 /* =========================
    UPLOAD AVATAR
 ========================= */
--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 export const SUPABASE_URL = "https://ulfkjmdhryaulesxlbxf.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsZmtqbWRocnlhdWxlc3hsYnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MzU2NTIsImV4cCI6MjA5MzQxMTY1Mn0.RHtLzxyn7goc7P04txvWzzmJlsWnpwJPM7NH-w2i8CQ";
 export const VAPID_PUBLIC_KEY = "BL4oHR9A9IEWN1NsvBm8fjsWnA-ZvICVy3Sx-9SvB365URDsamvzPbr8Sqz2VJ9lGBe_yUeXso5IjLwOu05d3RY";
-export const ADMIN_ID = "1d860813-9363-498f-8c58-1f8ea5995272";
+export const ADMIN_ID = "";
 export const EDGE_FUNCTION_PUSH_URL = "https://ulfkjmdhryaulesxlbxf.supabase.co/functions/v1/sendPush-ts";
 
 export const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
