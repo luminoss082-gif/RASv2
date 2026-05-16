b945687c1f163082d97f8c553bd78127a580210b
Luminoss <luminoss082@gmail.com>
2026-05-16 22:21:36 +0200

    cc

--- js/profiles-list.js
+++ js/profiles-list.js
@@ -684,7 +684,7 @@ export function renderProfiles() {
         e.stopPropagation();
 
         window.open(
-          "https://paypal.me/jeffreygqadal1/5.00",
+          "https://paypal.me/jeffreygadal1/5.00",
           "_blank"
         );
 
